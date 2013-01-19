fab276b8bd8c2a1fe0c338016f36fd6f4da194f8
root <root@mehdi-HP-EliteBook-8530w.(none)>
2013-01-19 09:43:52 +0100

    modification of the sending packet : name of source added in begining of packet

--- server/Classes/Network.rb
+++ server/Classes/Network.rb
@@ -39,17 +39,22 @@ class           Network
 
   private
   def           readable(reads, acceptor)
+    src_id = "R2D2"
     reads.each do |client|
+      @clients.each_key do |key|
+        if (@clients[key][:socket].object_id.to_s == client.object_id.to_s)
+          src_id = key
+        end
+      end
       # New client
       if client == acceptor
         new_client(acceptor)
       elsif client.eof?
-        disconnect_client(client)
+        disconnect_client(client, src_id)
         # Readable socket
       else
         cmd = client.gets("\n")
-        p "Received: " + cmd
-        handler_function(cmd, client)
+        handler_function(cmd, src_id)
       end
     end
   end
@@ -58,8 +63,6 @@ class           Network
     if (@packets.empty? == false)
       @clients.each_key do |key|
         @packets.each_index do |i|
-          puts "fds->packet(#{@packets[i][:id].to_s})"
-          puts "fds->client(#{@clients[key].to_s})"
           if (@packets[i][:id].to_s == key.to_s)
             @clients[key][:socket].puts(@packets[i][:content])
             @packets.delete_at(i)
@@ -77,18 +80,24 @@ class           Network
     @id_users += 1
   end
 
-  def           disconnect_client(client)
+  def           disconnect_client(client, src_id)
     puts "Bye bye !"
     @fds.delete(client)
+    @clients.delete(src_id)
     client.close    
   end
 
-  def handler_function(cmd, client)
+  def           handler_function(cmd, src_id)
     data = cmd.split(/\.?\s+/, 2)
     if (data[0].empty? == false)
       if (@handler_function.respond_to?(data[0]))
-        @packets = @handler_function.send(data[0].to_sym, client,
-                                          @clients, @packets, data)
+        if (data[0] == "broadcast_message")
+          @packets = @handler_function.send(data[0].to_sym, src_id,
+                                            @clients, @packets, data)
+        else
+          @packets, @clients = @handler_function.send(data[0].to_sym, src_id,
+                                                      @clients, @packets, data)
+        end
       else
         puts "Command #{data[0]} doesn't exist !"
       end
--- server/Classes/Protocol.rb
+++ server/Classes/Protocol.rb
@@ -9,10 +9,25 @@ class           Protocol
     puts "handler command is now ready"
   end
 
-  def           broadcast_message(client, clients, packets, data)
+  def           nick(src_id, clients, packets, data)
+    puts "Nick function"
     if (data.length == 2)
       clients.each_key do |key|
         packets << Packet.new(key, "toto42")
+      end
+      return packets, clients
+    else
+      puts "Usage: nick USERNAME"
+    end    
+  end
+
+  def           broadcast_message(src_id, clients, packets, data)
+    puts "broadcast_message function"
+    if (data.length == 2)
+      clients.each_key do |key|
+        if (key != src_id)
+          packets << Packet.new(key, "#{clients[src_id][:name]}: #{data[1]}")
+        end
       end
       return packets
     else
